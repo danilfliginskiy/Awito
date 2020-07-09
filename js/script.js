1b1120cf54a4fe0ae90e1c0178364dcf257881b3
danilfliginskiy <58595331+danilfliginskiy@users.noreply.github.com>
2020-07-09 16:59:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,51 +1,77 @@
 'use strict';
 
-const modalAdd       = document.querySelector('.modal__add'),
-      addAd          = document.querySelector('.add__ad'),
-      modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-      modalSubmit    = document.querySelector('.modal__submit'),
-      modalItem      = document.querySelector('.modal__item'),
-      cards          = document.querySelectorAll('.card'),
-      modals         = document.querySelectorAll('.modal');
+const dataBase = [];
 
-//closing the modal window by pressing esc
+const modalAdd        = document.querySelector('.modal__add'),
+      addAd           = document.querySelector('.add__ad'),
+      modalBtnSubmit  = document.querySelector('.modal__btn-submit'),
+      modalSubmit     = document.querySelector('.modal__submit'),
+      modalItem       = document.querySelector('.modal__item'),
+      modalBtnWarning = document.querySelector('.modal__btn-warning'),
+      catalog         = document.querySelector('.catalog'),
+      modals          = document.querySelectorAll('.modal');
 
-document.body.addEventListener('keydown', event => {
-    if (event.keyCode === 27) {
+//work with modals
+
+const elementsModalSubmit = [...modalSubmit.elements]
+    .filter(elem => elem.tagName !== 'BUTTON');
+
+modalSubmit.addEventListener('input', () => {
+
+    const validForm = elementsModalSubmit.every(elem => elem.value);
+    modalBtnSubmit.disabled = !validForm;
+    modalBtnWarning.style.display = validForm ? 'none' : '';
+
+});
+
+modalSubmit.addEventListener('submit', event => {
+
+    event.preventDefault();
+    const itemObject = {};
+    for (const elem of elementsModalSubmit) {
+        itemObject[elem.name] = elem.value;
+    }
+
+    dataBase.push(itemObject);
+    modalSubmit.reset();
+    modalBtnSubmit.disabled = true;
+    modalBtnWarning.style.display = 'flex';
+
+});
+
+//closing the modals
+
+const closeModal = function(event) {
+
+    const target = event.target;
+
+    if (target.closest('.modal__close') || target === modalAdd || target === modalItem || event.code === 'Escape') {
         modalItem.classList.add('hide');
         modalAdd.classList.add('hide');
+        modalSubmit.reset();
+        document.body.removeEventListener('keydown', closeModal);
     }
-});
+
+};
+
+modalAdd.addEventListener('click', closeModal);
+modalItem.addEventListener('click', closeModal);
 
 //open modal windows
 
 addAd.addEventListener('click', () => {
     modalAdd.classList.remove('hide');
     modalBtnSubmit.disabled = true;
+    document.body.addEventListener('keydown', closeModal);
 });
 
-cards.forEach(item => {
+catalog.addEventListener('click', event => {
 
-    item.addEventListener('click', () => {
+    const target = event.target;
+
+    if(target.closest('.card')) {
         modalItem.classList.remove('hide');
-    });
-
-});
-
-//close modal windows by cross and overlay
-
-modals.forEach(item => {
-
-    item.addEventListener('click', event => {
-
-        const target = event.target;
-    
-        if(target.classList.contains('modal__close') || target === modalAdd || target === modalItem) {
-            modalAdd.classList.add('hide');
-            modalItem.classList.add('hide');
-            modalSubmit.reset();
-        }
-    
-    });
+        document.body.addEventListener('keydown', closeModal);
+    }
 
 });
