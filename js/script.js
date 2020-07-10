eac7af7bdcb1a7c0b0e03bae8e4fa09b39fe75b0
danilfliginskiy <58595331+danilfliginskiy@users.noreply.github.com>
2020-07-10 14:11:44 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const dataBase = [];
+const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
 
 const modalAdd        = document.querySelector('.modal__add'),
       addAd           = document.querySelector('.add__ad'),
@@ -9,20 +9,118 @@ const modalAdd        = document.querySelector('.modal__add'),
       modalItem       = document.querySelector('.modal__item'),
       modalBtnWarning = document.querySelector('.modal__btn-warning'),
       catalog         = document.querySelector('.catalog'),
-      modals          = document.querySelectorAll('.modal');
+      modalFileInput  = document.querySelector('.modal__file-input'),
+      modalFileBtn    = document.querySelector('.modal__file-btn'),
+      modalImageAdd   = document.querySelector('.modal__image-add');
 
-//work with modals
+const textFileBtn   = modalFileBtn.textContent,
+      srcModalImage = modalImageAdd.src;
 
-const elementsModalSubmit = [...modalSubmit.elements]
-    .filter(elem => elem.tagName !== 'BUTTON');
+//render card
 
-modalSubmit.addEventListener('input', () => {
+const renderCard = () => {
 
+    catalog.textContent = '';
+
+    dataBase.forEach((item, i) => {
+        catalog.insertAdjacentHTML('beforeend', `
+            <li class="card" data-id="${i}">
+                <img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
+                <div class="card__description">
+                    <h3 class="card__header">${item.nameItem}</h3>
+                    <div class="card__price">${item.costItem}</div>
+                </div>
+            </li>
+        `);
+    });
+
+}
+
+//render modals
+
+const renderModal = (e) => {
+    modalItem.textContent = '';
+  
+    dataBase.forEach((item, i) => {
+      if (i === +e) {
+        if (item.status == 'old') {
+            item.status = 'Б/У';
+        } else if (item.status == 'new') {
+            item.status = 'Новый';
+        }
+        modalItem.insertAdjacentHTML('beforeend', `   
+          <div class="modal__block">
+            <h2 class="modal__header">Купить</h2>
+            <div class="modal__content">
+              <div>
+                <img class="modal__image modal__image-item" src="data:image/jpeg;base64,${item.image}" alt="test"/>
+              </div>
+              <div class="modal__description">
+                <h3 class="modal__header-item">${item.nameItem}</h3>
+                <p>
+                  Состояние:
+                  <span class="modal__status-item">${item.status}</span>
+                </p>
+                <p>
+                  Описание:
+                  <span class="modal__description-item">${item.descriptionItem}</span>
+                </p>
+                <p>
+                  Цена:
+                  <span class="modal__cost-item">${item.costItem} ₽</span>
+                </p>
+                <button class="btn">Купить</button>
+              </div>
+            </div>
+            <button class="modal__close">&#10008;</button>
+          </div>   
+        `);
+      }
+    });
+  };
+
+//work with data base and photo
+
+const infoPhoto = {};
+
+const saveData = () => localStorage.setItem('awito', JSON.stringify(dataBase));
+
+modalFileInput.addEventListener('change', event => {
+
+    const target = event.target;
+    const reader = new FileReader();
+    const file = target.files[0];
+
+    infoPhoto.filename = file.name;
+    infoPhoto.size = file.size;
+
+    reader.readAsBinaryString(file);
+
+    reader.addEventListener('load', event => {
+        if (infoPhoto.size < 200000) {
+            modalFileBtn.textContent = infoPhoto.filename;
+            infoPhoto.base64 = btoa(event.target.result);
+            modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`;
+        } else {
+            modalFileBtn.textContent = 'Слишком большое фото (более 200KB)';
+            modalFileInput.value = '';
+            checkForm();
+        }
+    });
+
+});
+
+//work with submit modal
+
+const elementsModalSubmit = [...modalSubmit.elements].filter(elem => elem.tagName !== 'BUTTON');
+
+const checkForm = () => {
     const validForm = elementsModalSubmit.every(elem => elem.value);
     modalBtnSubmit.disabled = !validForm;
     modalBtnWarning.style.display = validForm ? 'none' : '';
+}
 
-});
+modalSubmit.addEventListener('input', checkForm);
 
 modalSubmit.addEventListener('submit', event => {
 
@@ -32,23 +130,27 @@ modalSubmit.addEventListener('submit', event => {
         itemObject[elem.name] = elem.value;
     }
 
+    itemObject.image = infoPhoto.base64;
     dataBase.push(itemObject);
-    modalSubmit.reset();
-    modalBtnSubmit.disabled = true;
-    modalBtnWarning.style.display = 'flex';
+    closeModal({target: modalAdd});
+    saveData();
+    renderCard();
 
 });
 
 //closing the modals
 
-const closeModal = function(event) {
+const closeModal = event => {
 
     const target = event.target;
 
-    if (target.closest('.modal__close') || target === modalAdd || target === modalItem || event.code === 'Escape') {
+    if (target.closest('.modal__close') || target.classList.contains('modal') || event.code === 'Escape') {
         modalItem.classList.add('hide');
         modalAdd.classList.add('hide');
         modalSubmit.reset();
+        modalImageAdd.src = srcModalImage;
+        modalFileBtn.textContent = textFileBtn;
+        checkForm();
         document.body.removeEventListener('keydown', closeModal);
     }
 
@@ -70,8 +172,11 @@ catalog.addEventListener('click', event => {
     const target = event.target;
 
     if(target.closest('.card')) {
+        renderModal(target.closest('.card').dataset.id);
         modalItem.classList.remove('hide');
         document.body.addEventListener('keydown', closeModal);
     }
 
 });
+
+renderCard();
